05ba3f31f0c5d595eee2a637a3d06f1d007450d5
Alicja K <akwiaton@gmail.com>
2024-07-30 11:34:26 +0200

    dashboard & user component added, routingLink set

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
+import { RouterModule } from '@angular/router';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatToolbarModule, MatSidenavModule, MatIconModule],
+  imports: [RouterOutlet, MatToolbarModule, MatSidenavModule, MatIconModule, RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
